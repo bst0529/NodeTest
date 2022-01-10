7c5a8c4b59f7239c2e66fde4fb8d0fa3633eabdb
bst0529 <bst0529@hotmail.com>
2022-01-11 00:41:44 +0800

    電影清單：頁面顯示資料 至U47:動態路由

---
+++ movie_list/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs.engine({ extname: '.handlebars', defaultLayout: "main" }));
+app.set('view engine', 'handlebars')
+
+const movieList = require('./movies.json')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { movies: movieList.results })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  let movieOne = movieList.results.find(movie => movie.id.toString() === req.params.movie_id)
+  res.render('show', { movie: movieOne })
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
