1349c1ea4a29bdec1b8d5d524c7d84f0c51a2651
Liu Chun-Kai <bst0529@hotmail.com>
2022-01-11 13:22:01 +0800

    S2 Express 網路應用程式 電影清單：輪到你了 A1: 打造餐廳清單：缺README.md

---
+++ restaurant_list/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs.engine({ extname: '.handlebars', defaultLayout: "main"}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+const restaurantList = require('./restaurant.json')
+
+app.get('/', (req, res) => {
+    res.render('index',{restaurants: restaurantList.results})
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+    var oneRestaurant = restaurantList.results.find(x => x.id.toString() === req.params.restaurant_id)
+    res.render('show',{restaurant: oneRestaurant})
+})
+
+app.get('/Search', (req, res) => {
+    let keyword = req.query.keyword
+    let restaurants = restaurantList.results.filter(restaurant => {return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())})
+    res.render('index',{restaurants: restaurants,keyword: keyword})
+})
+
+app.listen(port, () => {
+    console.log(`Express is listening on http://localhost:${port}`)
+})
