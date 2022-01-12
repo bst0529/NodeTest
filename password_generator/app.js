9a72e04fd5267921e278f0025b8d1cad9ddc2c29
bst0529 <bst0529@hotmail.com>
2022-01-12 23:45:26 +0800

    1.U51_HTML 表單元素與實作技巧 2.密碼產生器 U52~U53

---
+++ password_generator/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: "main" }))
+app.set('view engine', 'handlebars')
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`The Express Server is running on http://localhost:${port}`)
+})
