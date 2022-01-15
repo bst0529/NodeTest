d3fc949e72eb1cf01d1fb0b4b3b231d11f0477dd
bst0529 <bst0529@hotmail.com>
2022-01-15 21:20:18 +0800

    feat: modify index.hbs

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const Todo = require('./models/todo')
 const app = express()
 const port = 3000
 
@@ -20,7 +21,14 @@ app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  // 取出 Todo model 裡的所有資料
+  Todo.find()
+    // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .lean()
+    // 將資料傳給 index 樣板
+    .then(todos => res.render('index', { todos }))
+    // 錯誤處理
+    .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
