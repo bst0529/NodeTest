fc94748d3aedb73f11fcfd6083a42536d24ba8f0
bst0529 <bst0529@hotmail.com>
2022-01-15 20:03:42 +0800

    feat: add mongoose

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost/todo-list')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!!!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!!!')
+})
 
 app.get('/', (req, res) => {
   res.send('This is Root')
