beba41ab810ea535d76ac67fa7dddf76529a9cad
bst0529 <bst0529@hotmail.com>
2022-01-17 21:44:27 +0800

    feat: 建立初始環境

---
+++ restaurant_list_MongoDB/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`This Express Server is running on http://localhost:${port}`)
+})
