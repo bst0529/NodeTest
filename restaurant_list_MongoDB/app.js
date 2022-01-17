0e15a8e4282b0d64ade49d41dfb0ef25dda91ce3
bst0529 <bst0529@hotmail.com>
2022-01-17 22:26:21 +0800

    fix: body-parser的引用修正

--- restaurant_list_MongoDB/app.js
+++ restaurant_list_MongoDB/app.js
@@ -5,10 +5,10 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
