1c49720d58d02b75e073f0f60b13a48891aa106d
Liu Chun-Kai <bst0529@hotmail.com>
2022-01-25 09:28:32 +0800

    refactor: modify mongoose config

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,24 +1,14 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
+
 const Todo = require('./models/todo')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const router = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
-
-mongoose.connect('mongodb://localhost/todo-list')
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!!!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!!!')
-})
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
